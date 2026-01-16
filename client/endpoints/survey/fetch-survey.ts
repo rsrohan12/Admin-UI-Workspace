ffd9c2635132f2a2adf667b7364481126ad38344
Rohan <rsrohan787@gmail.com>
2026-01-16 15:44:47 +0530

    minor fix

--- client/endpoints/survey/fetch-survey.ts
+++ client/endpoints/survey/fetch-survey.ts
@@ -4,20 +4,18 @@ import { useGlobalLoader } from "@/hooks";
 export const GET_SURVEY_KEY = "fetch-survey";
 
 export const fetchSurveyList = async (payload: any) => {
-  // try {
-  //     const response = await backendClient.get<
-  //       TServerResponseWithPagination<TUser>
-  //     >(
-  //       `/admin/user/list?size=${payload?.size}&skip=${payload?.skip}&search=${
-  //         payload?.search
-  //       }&sorting=${payload?.sorting ?? ''}&trashOnly=${
-  //         payload?.trashOnly ?? ''
-  //       }`,
-  //     );
-  //     return response?.data?.data;
-  //   } catch (error) {
-  //     throw error;
-  //   } finally {
-  //     useGlobalLoader.getState().setShowLoader(false);
-  //   }
+  try {
+      const response = await backendClient.get(
+        `/admin/user/list?size=${payload?.size}&skip=${payload?.skip}&search=${
+          payload?.search
+        }&sorting=${payload?.sorting ?? ''}&trashOnly=${
+          payload?.trashOnly ?? ''
+        }`,
+      );
+      return response?.data?.data;
+    } catch (error) {
+      throw error;
+    } finally {
+      useGlobalLoader.getState().setShowLoader(false);
+    }
 };
