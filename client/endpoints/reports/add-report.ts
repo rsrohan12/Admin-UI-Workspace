2e11c76bbb4c01230c042b464c7bbed561692982
Rohan <rsrohan787@gmail.com>
2026-01-18 22:57:29 +0530

    added endpoints

---
+++ client/endpoints/reports/add-report.ts
@@ -0,0 +1,21 @@
+import { backendClient } from "@/client/backendClient";
+import { useGlobalLoader } from "@/hooks";
+
+export const ADD_REPORT_KEY = "add-report";
+
+export type TAddReport = {
+  block_name: string;
+  sort_number: string;
+};
+
+export const addReportRequest = async (payload: TAddReport) => {
+  useGlobalLoader.getState().setShowLoader(true);
+  try {
+    // const response = await backendClient.post("/admin/user/add", payload);
+    // return response;
+  } catch (error) {
+    throw error;
+  } finally {
+    useGlobalLoader.getState().setShowLoader(false);
+  }
+};
---
+++ client/endpoints/survey/add-survey.ts
@@ -0,0 +1,21 @@
+import { backendClient } from "@/client/backendClient";
+import { useGlobalLoader } from "@/hooks";
+
+export const ADD_SURVEY_KEY = "add-survey";
+
+export type TAddSurvey = {
+  block_name: string;
+  sort_number: string;
+};
+
+export const addSurveyRequest = async (payload: TAddSurvey) => {
+  useGlobalLoader.getState().setShowLoader(true);
+  try {
+    // const response = await backendClient.post("/admin/user/add", payload);
+    // return response;
+  } catch (error) {
+    throw error;
+  } finally {
+    useGlobalLoader.getState().setShowLoader(false);
+  }
+};
