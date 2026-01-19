87c3a50bc8d0398c362fd2afce424420178fb6ee
Rohan <rsrohan787@gmail.com>
2026-01-19 23:17:01 +0530

    delete endpoints added

---
+++ client/endpoints/survey/delete-survey.ts
@@ -0,0 +1,18 @@
+import { backendClient } from '@/client/backendClient';
+import { useGlobalLoader } from '@/hooks';
+
+export const DELETE_SURVEY_KEY = 'delete-survey';
+
+export const deleteSurveyRequest = async (ids: number[]) => {
+  useGlobalLoader.getState().setShowLoader(true);
+  try {
+    // const response = await backendClient.delete(`/admin/user/delete`, {
+    //   data: { ids },
+    // });
+    // return response;
+  } catch (error) {
+    throw error;
+  } finally {
+    useGlobalLoader.getState().setShowLoader(false);
+  }
+};
