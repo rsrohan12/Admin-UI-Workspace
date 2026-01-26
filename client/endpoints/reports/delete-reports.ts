da912a237a85c405d258dafc42cd991e0a58b92c
Rohan <rsrohan787@gmail.com>
2026-01-26 21:57:20 +0530

    added api url

--- client/endpoints/reports/delete-reports.ts
+++ client/endpoints/reports/delete-reports.ts
@@ -6,10 +6,10 @@ export const DELETE_REPORT_KEY = 'delete-report';
 export const deleteReportRequest = async (ids: number[]) => {
   useGlobalLoader.getState().setShowLoader(true);
   try {
-    // const response = await backendClient.delete(`/admin/user/delete`, {
-    //   data: { ids },
-    // });
-    // return response;
+    const response = await backendClient.delete(`/admin/user/delete`, {
+      data: { ids },
+    });
+    return response;
   } catch (error) {
     throw error;
   } finally {
--- client/endpoints/survey/delete-survey.ts
+++ client/endpoints/survey/delete-survey.ts
@@ -6,10 +6,10 @@ export const DELETE_SURVEY_KEY = 'delete-survey';
 export const deleteSurveyRequest = async (ids: number[]) => {
   useGlobalLoader.getState().setShowLoader(true);
   try {
-    // const response = await backendClient.delete(`/admin/user/delete`, {
-    //   data: { ids },
-    // });
-    // return response;
+    const response = await backendClient.delete(`/admin/user/delete`, {
+      data: { ids },
+    });
+    return response;
   } catch (error) {
     throw error;
   } finally {
