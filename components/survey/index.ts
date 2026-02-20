5fc86eeb544d2de8884aed88c139b4c9e6573d89
Rohan <rsrohan787@gmail.com>
2026-02-20 14:00:48 +0530

    commented, will use later

--- components/survey/index.ts
+++ components/survey/index.ts
@@ -1,4 +1,4 @@
-export * from './list';
-export * from './add';
-// export * from './edit';
-// export * from './trash-list';
+// export * from './list';
+// export * from './add';
+// // export * from './edit';
+// // export * from './trash-list';
